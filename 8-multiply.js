c81a94b9d160e6a5fbbb4834ce1e90479cb18ee9
Brendon DSouza <brendonmufc@gmail.com>
2022-03-25 16:24:47 -0400

    daily challenge

---
+++ 8-multiply.js
@@ -0,0 +1,17 @@
+//////////////////////////////////////////
+// Count the divisors of a number
+//////////////////////////////////////////
+
+
+// URL
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
+
+// INSTRUCTIONS
+// This code does not execute properly. Try to figure out why.
+
+// Examples 
+
+function multiply(a, b){
+  return a * b
+}
