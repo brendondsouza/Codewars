dcb620fb6de052f3aacf93599918ee9d5705a933
Brendon DSouza <brendonmufc@gmail.com>
2022-04-04 17:05:55 -0400

    formatting

--- 7-kyu/descending-order.js
+++ 7-kyu/descending-order.js
@@ -2,14 +2,11 @@
 Title
 Descending Order
 
-
 URL
 https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 
-
 INSTRUCTIONS
 Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
-
 
 Examples 
 Input: 42145 Output: 54421
