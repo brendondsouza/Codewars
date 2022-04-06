ec223eee3ba98539093c408c1582a0191629cb4c
Brendon DSouza <brendonmufc@gmail.com>
2022-04-05 21:50:17 -0400

    initial commit

--- 8-kyu/sum-of-strings.js
+++ 8-kyu/sum-of-strings.js
@@ -1,13 +1,18 @@
 //////////////////////////////////////////
-// 
+// Sum The Strings
 //////////////////////////////////////////
 
 
 // URL
-// 
+// https://www.codewars.com/kata/5966e33c4e686b508700002d
 
 
 // INSTRUCTIONS
-// 
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
 
 // Examples 
+// "4",  "5" --> "9"
+
+function sumStr(a,b) {
+  return String(+a + +b)
+}
