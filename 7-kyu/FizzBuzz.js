f712654911e7116cc70f8fc263d736cf8f513c27
Brendon DSouza <brendonmufc@gmail.com>
2022-04-26 20:24:38 -0400

    initial commit

---
+++ 7-kyu/FizzBuzz.js
@@ -0,0 +1,28 @@
+/* TITLE
+Fizz Buzz
+
+URL
+https://www.codewars.com/kata/5300901726d12b80e8000498
+
+INSTRUCTIONS
+Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+N will never be less than 1.
+
+
+EXAMPLES
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+*/
+function fizzbuzz(n){
+  let arr = []
+  for(let i = 1; i<=n; i++){
+    arr.push(i)
+  }
+  return arr.map(n=> n%15===0? 'FizzBuzz': n%3==0?'Fizz':n%5===0?'Buzz':n)
+}
